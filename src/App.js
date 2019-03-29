1d944a7270abe2858d005ecd9f9568bbfbf96216
Oscar Armando Cruz Mendez <oscar.cruz@uprivada.edu>
2019-03-29 09:41:08 -0700

    tutorial app react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navigation from './components/navigation';
 import './App.css';
+import { Task } from './task.json';
+import TaksForm from './components/TaksForm';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      Task
+    }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  handleAddTodo(task) {
+    this.setState({
+      Task: [...this.state.Task, task]
+    })
+  }
+
+  removeTodo(index) {
+    this.setState({
+      Task: this.state.todos.filter((e, i) => {
+        return i !== index
+      })
+    });
+  }
+
   render() {
+    const tasks = this.state.Task.map((task, i) => {
+      return (
+       <div className="col-md-4">
+           <div className= "card mt-4">  
+          <div className="card-header">
+            <h3>{task.title}</h3>
+            <span className="badge badge-pill badge-danger ml-2">
+              {task.priority}
+            </span>
+          </div>
+          <div class className="card-body">
+            <p>{task.description}</p>
+            <p><mark>{task.responsible}</mark></p>
+          </div>
+          <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}>
+                Delete
+              </button>
+            </div>
+        </div>
+       </div>
+      )
+    })
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Navigation counter={this.state.Task.length}/>
+      <div className="container"> 
+        <di className="row mt-4">
+        <div className="col-md-4 text-center">
+              <TaksForm onAddTodo={this.handleAddTodo}></TaksForm>
+            </div>
+            <div className="col-md-8">
+              <div className="row">
+              {tasks}
+              </div>
+            </div>
+
+        </di>
+      </div>
       </div>
     );
   }
